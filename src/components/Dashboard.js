b82cb190ee4bbb181ee928011ace8d207e3566cc
moeinFarzin <moein.akd12345@gmail.com>
2022-09-11 12:02:06 -0700

    initial project

---
+++ src/components/Dashboard.js
@@ -0,0 +1,10 @@
+
+const Dashboard =()=>{
+
+    return(
+        <>
+        <h1>reg</h1>
+        </>
+    )
+}
+export default Dashboard
---
+++ src/components/Login.js
@@ -0,0 +1,10 @@
+
+const Login =()=>{
+
+    return(
+<>
+<h1>erv</h1>
+</>
+    )
+}
+export default Login
